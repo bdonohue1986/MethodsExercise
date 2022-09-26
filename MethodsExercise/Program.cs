6bc5b26b9f127238b951df0a1dea06cd5cfdcb64
bdonohue1986 <100311620+bdonohue1986@users.noreply.github.com>
2022-09-26 17:56:58 -0400

    inital commit

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Console.WriteLine("Hello, can we have your first name please?");
+            var userName = Console.ReadLine();
+            Console.WriteLine($"Hi {userName}, whats your Favorite Color?");
+            var favoriteColor = Console.ReadLine();
+            Console.WriteLine($"And {userName} what is your favorite band");
+            var favBand = Console.ReadLine();
+            Console.WriteLine($"And whats your favorite animal {userName}?");
+            var favAnimal = Console.ReadLine();
+            Console.WriteLine($"Quick question {userName} has listening to {favBand} ever made you see a {favoriteColor} {favAnimal}?");
+         }
+        public static int Add(int num1, int num2) 
+            { return num1 + num2; }
+        public static int Multiply(int num1, int num2)
+            { return num1 * num2; }
+
     }
+
 }
